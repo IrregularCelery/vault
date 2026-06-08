1ab91ee3129abf310faa5c891e0b92c7210629bf
Mohsen Rad <irregular.celery@gmail.com>
2026-06-08 15:05:41 +0330

    [`vault`]: Add `Backend` trait for the `storage` module

--- vault/src/lib.rs
+++ vault/src/lib.rs
@@ -1,3 +1,4 @@
 #![no_std]
 
 pub mod crypto;
+pub mod storage;
---
+++ vault/src/storage/backend.rs
@@ -0,0 +1,40 @@
+use gate::sys::{
+    io,
+    string::{String, ToString},
+    vec::Vec,
+};
+
+#[derive(Debug)]
+pub enum Error {
+    NotFound,
+    Io(String),
+    Other(String),
+}
+
+impl core::fmt::Display for Error {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        match self {
+            Error::NotFound => write!(f, "blob not found"),
+            Error::Io(e) => write!(f, "I/O error: {}", e),
+            Error::Other(e) => write!(f, "storage error: {}", e),
+        }
+    }
+}
+
+impl From<io::Error> for Error {
+    fn from(value: gate::sys::io::Error) -> Self {
+        if value.kind() == io::ErrorKind::NotFound {
+            return Self::NotFound;
+        }
+
+        Self::Io(value.to_string())
+    }
+}
+
+pub trait Backend {
+    fn put(&self, hash: &[u8; 32], data: &[u8]) -> Result<(), Error>;
+    fn get(&self, hash: &[u8; 32]) -> Result<Vec<u8>, Error>;
+    fn exists(&self, hash: &[u8; 32]) -> Result<bool, Error>;
+    fn delete(&self, hash: &[u8; 32]) -> Result<(), Error>;
+    fn list(&self) -> Result<Vec<[u8; 32]>, Error>;
+}
