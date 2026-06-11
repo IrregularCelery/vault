ac9121f74bb200e751889171e75573d9c35968d6
Mohsen Rad <irregular.celery@gmail.com>
2026-06-11 18:10:12 +0330

    [`vault/storage`]: rename `ChunkReader` to `Chunks` for `chunk` module

--- crates/vault/src/storage/chunk.rs
+++ crates/vault/src/storage/chunk.rs
@@ -1,4 +1,7 @@
-use gate::sys::{io, macros::vec, vec::Vec};
+use gate::{
+    crypto::blake3,
+    sys::{io, macros::vec, vec::Vec},
+};
 
 pub const CHUNK_SIZE: usize = 4 * 1024 * 1024; // Each chunk's max capacity is 4 MiB
 
@@ -8,23 +11,36 @@ pub enum Error {
     Other(&'static str),
 }
 
+impl core::fmt::Display for Error {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        match self {
+            Error::Io(e) => write!(f, "I/O: {}", e),
+            Error::Other(e) => write!(f, "{}", e),
+        }
+    }
+}
+
 #[derive(Debug)]
 pub struct Chunk<'a> {
-    data: &'a [u8],
+    pub data: &'a [u8],
 }
 
 impl<'a> Chunk<'a> {
     pub fn new(data: &'a [u8]) -> Self {
         Self { data }
     }
+
+    pub fn address(&self, encryption_key: &[u8; 32]) -> [u8; 32] {
+        *blake3::keyed_hash(encryption_key, self.data).as_bytes()
+    }
 }
 
-pub struct ChunkReader<R: io::Read> {
+pub struct Chunks<R: io::Read> {
     reader: R,
     buf: Vec<u8>,
 }
 
-impl<R: io::Read> ChunkReader<R> {
+impl<R: io::Read> Chunks<R> {
     pub fn new(reader: R) -> Self {
         Self {
             reader,
